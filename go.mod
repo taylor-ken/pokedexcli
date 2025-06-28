b932793bd9dacf6c23584c1bbc36ffdc42663965
taylor-ken <kenneth.n.taylor@gmail.com>
2025-06-28 12:22:27 -0400

    repl

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/taylor-ken/pokedexcli
+
+go 1.24.3
---
+++ repl_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "  Charmander Bulbasaur PIKACHU  ",
+			expected: []string{"charmander", "bulbasaur", "pikachu"},
+		},
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		if len(actual) != len(c.expected) {
+			t.Errorf("Slice length mismatch: expected %d, got %d", len(c.expected), len(actual))
+		}
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			if word != expectedWord {
+				t.Errorf("Words don't match: expected %s, got %s", word, expectedWord)
+			}
+		}
+	}
+}
