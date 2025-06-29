fb6d19426eab5efef2c0576b6b903bcbeb11589c
taylor-ken <kenneth.n.taylor@gmail.com>
2025-06-29 08:31:51 -0400

    repl

--- main.go
+++ main.go
@@ -1,22 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-
-	for {
-		fmt.Print("Pokedex > ")
-		if scanner.Scan() {
-			line := scanner.Text()
-			cleaned := cleanInput(line)
-			if len(cleaned) > 0 {
-				fmt.Printf("Your command was: %s\n", cleaned[0])
-			}
-		}
-	}
+	startRepl()
 }
--- repl_test.go
+++ repl_test.go
@@ -10,25 +10,34 @@ func TestCleanInput(t *testing.T) {
 		expected []string
 	}{
 		{
+			input:    "  ",
+			expected: []string{},
+		},
+		{
+			input:    "  hello  ",
+			expected: []string{"hello"},
+		},
+		{
 			input:    "  hello  world  ",
 			expected: []string{"hello", "world"},
 		},
 		{
-			input:    "  Charmander Bulbasaur PIKACHU  ",
-			expected: []string{"charmander", "bulbasaur", "pikachu"},
+			input:    "  HellO  World  ",
+			expected: []string{"hello", "world"},
 		},
 	}
 
 	for _, c := range cases {
 		actual := cleanInput(c.input)
 		if len(actual) != len(c.expected) {
-			t.Errorf("Slice length mismatch: expected %d, got %d", len(c.expected), len(actual))
+			t.Errorf("lengths don't match: '%v' vs '%v'", actual, c.expected)
+			continue
 		}
 		for i := range actual {
 			word := actual[i]
 			expectedWord := c.expected[i]
 			if word != expectedWord {
-				t.Errorf("Words don't match: expected %s, got %s", word, expectedWord)
+				t.Errorf("cleanInput(%v) == %v, expected %v", c.input, actual, c.expected)
 			}
 		}
 	}
